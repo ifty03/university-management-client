f5bfa2c336aeb7c8d02dc4a25623f52021c7a998
ifty03 <ashikulislamb84@gmail.com>
2022-07-13 10:41:59 +0600

    navbar added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../Assets/Images/logo.png";
+
+const Navbar = () => {
+  return (
+    <div class="navbar bg-base-100 flex justify-between lg:px-14 md:px-10 px-3">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabindex="0">
+              <a class="justify-between">
+                Parent
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul class="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img className="w-20" src={logo} alt="logo" />
+        </div>
+      </div>
+      <div class="navbar-end font-semibold">
+        <ul class="menu menu-horizontal p-0 hidden lg:flex">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li tabindex="0">
+            <a>
+              Parent
+              <svg
+                class="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul class="p-2">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+        <div className="flex items-center">
+          <Link
+            to="/"
+            className="bg-green-600 hover:bg-green-500 text-white shadow-green-300 shadow-lg px-4 py-[7px] mx-2 rounded-md hidden md:flex"
+          >
+            Get Started
+          </Link>
+          <label tabindex="0" class="btn btn-ghost btn-circle ml-4 avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
