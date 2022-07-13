5522d7032a805da1532eb9365e774636172d32df
ifty03 <ashikulislamb84@gmail.com>
2022-07-13 16:06:08 +0600

    sign up component added

--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -11,9 +11,9 @@ const SignUp = () => {
       }}
     >
       <div class="hero-overlay bg-opacity-60"></div>
-      <div class="hero-content grid grid-cols-2 gap-20 text-white">
-        <div class="p-14 bg-opacity-30 max-w-lg rounded-lg bg-slate-700">
-          <h1 class="mb-5 text-5xl text-white font-bold">
+      <div class="hero-content grid lg:grid-cols-2 grid-cols-1 lg:gap-20 gap-10 text-white">
+        <div class="lg:p-14 p-5 bg-opacity-30 max-w-lg rounded-lg bg-slate-700">
+          <h1 class="mb-5 text-4xl text-white font-bold">
             Learn From The <span className="text-green-500">Expert</span>
           </h1>
           <p>
@@ -26,9 +26,49 @@ const SignUp = () => {
           </div>
         </div>
         <div>
-          <h2 className="text-3xl">Get Started With Me !</h2>
-          <p className="opacity-50">Please Sign Up here !</p>
-          <Social />
+          <h2 className="text-3xl mb-2 pl-5">Get Started With Me 🔐</h2>
+          <p className="opacity-50 mb-8 pl-5">Please Sign Up here !</p>
+          <div className="bg-opacity-30 p-5 rounded-lg bg-slate-700">
+            <Social />
+            <div className="flex items-center gap-3 text-lg my-5">
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              or
+              <div className="w-full h-[1px] bg-gray-400"></div>
+            </div>
+            {/* sign up from */}
+            <div class="card flex-shrink-0 w-full">
+              <div class="card-body">
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text text-white">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    class="input border border-slate-400 bg-slate-800  text-white"
+                  />
+                </div>
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text text-white">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="password"
+                    class="input border border-slate-400 bg-slate-800  text-white"
+                  />
+                  <label class="label">
+                    <a href="#" class="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div class="form-control mt-6">
+                  <button class="btn btn-primary">Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 const Social = () => {
   return (
-    <div>
-      <FcGoogle />
+    <div className="flex justify-between items-center text-3xl">
+      <FcGoogle className="border-slate-500 border md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 cursor-pointer" />
+      <FaFacebook className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 text-blue-500 cursor-pointer" />
+      <AiFillGithub className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 hover:text-black cursor-pointer" />
     </div>
-);
+  );
 };
-export default Social
+export default Social;
