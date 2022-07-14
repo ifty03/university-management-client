f96befd27a2561ae3e20c280ad08a877eec9cbf8
ifty03 <ashikulislamb84@gmail.com>
2022-07-14 19:15:48 +0600

    social auth added

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -2,12 +2,24 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../firebase.init";
 import { toast } from "react-hot-toast";
+import Loading from "../Shared/Loading/Loading";
 
 const Social = ({ setCustomError }) => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [signInWithFacebook, fUser, fLoading, fError] =
+    useSignInWithFacebook(auth);
+  const [signInWithGithub, gitUser, gitLoading, gitError] =
+    useSignInWithGithub(auth);
+  if (gLoading || fLoading || gitLoading) {
+    return <Loading />;
+  }
   return (
     <div className="flex justify-between items-center text-3xl">
       <FcGoogle
@@ -20,8 +32,26 @@ const Social = ({ setCustomError }) => {
         }}
         className="border-slate-500 border md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 cursor-pointer"
       />
-      <FaFacebook className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 text-blue-500 cursor-pointer" />
-      <AiFillGithub className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 hover:text-black cursor-pointer" />
+      <FaFacebook
+        onClick={async () => {
+          if (fError) {
+            setCustomError(fError?.message);
+          }
+          await signInWithFacebook();
+          toast.success("user created successfully !");
+        }}
+        className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 text-blue-500 cursor-pointer"
+      />
+      <AiFillGithub
+        onClick={async () => {
+          if (gitError) {
+            setCustomError(gitError?.message);
+          }
+          await signInWithGithub();
+          toast.success("user created successfully !");
+        }}
+        className="border border-slate-500 md:w-36 w-24 h-12 py-[5px] rounded-md hover:bg-green-200 hover:text-black cursor-pointer"
+      />
     </div>
   );
 };
